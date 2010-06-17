d7f45bc51d12c1e74fdb90fd9fb6d9b630da8ebb
Maxim Soldatov <fomaxi@gmail.com>
2010-06-17 04:04:41 +0400

    with options & marks matches

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,7 +14,8 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->lineEdit->setFocus();
-    ui->treeWidget->setColumnWidth(0, 270);
+    ui->treeWidget->setColumnWidth(0, 400);
+    process = NULL;
 }
 
 MainWindow::~MainWindow()
@@ -36,51 +37,71 @@ void MainWindow::changeEvent(QEvent *e)
 
 void MainWindow::on_pushButton_clicked()
 {
+    if (process != NULL && process->state() == QProcess::Running)
+    {
+        process->terminate();
+        return;
+    }
+
     search = ui->lineEdit->text();
 
     ui->treeWidget->clear();
+    ui->lineEdit->setDisabled(true);
+    ui->pushButton->setText("Stop");
     ui->statusBar->showMessage("Searching...");
 
-    QIcon icon = QFileIconProvider().icon(QString("/"));
-    root = new QTreeWidgetItem(ui->treeWidget);
-    root->setText(0, "/");
-    root->setIcon(0, icon);
-    ui->treeWidget->addTopLevelItem(root);
-    ui->treeWidget->expandItem(root);
+    timer.start();
+
+    root = addItem(NULL, QString("/"), QString("/"), false);
+
+    QStringList arguments;
+
+    if (ui->checkBox_basename->isChecked())
+        arguments << "--basename";
+    if (ui->checkBox_ignorecase->isChecked())
+        arguments << "--ignore-case";
+    if (ui->checkBox_regex->isChecked())
+        arguments << "--regex";
+    if (ui->checkBox_limit->isChecked())
+    {
+        arguments << "--limit";
+        arguments << ui->lineEdit_limit->text();
+    }
+
+    arguments << "--quiet";
+    arguments << search;
 
     process = new QProcess(this);
     connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(ReadStdoutOutput()));
     connect(process, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(finished()));
-    process->start("/usr/bin/locate", QStringList() << search, QIODevice::ReadOnly);
+    process->start("/usr/bin/locate", arguments, QIODevice::ReadOnly);
 }
 
 void MainWindow::ReadStdoutOutput()
 {
-    qDebug() << "ReadStdoutOutput()";
     while (process->canReadLine())
     {
         QByteArray path = process->readLine(1024).trimmed();
-        addPath(catPath(path));
+        addPath(path);
+        QCoreApplication::processEvents();
+    }
+
+    if (process->state() == QProcess::NotRunning)
+    {
+        ui->statusBar->showMessage(QString("Finish build tree: %1 ms").arg(timer.elapsed()));
     }
 }
 
 void MainWindow::finished()
 {
-    ui->statusBar->showMessage("Finish");
-}
-
-QString MainWindow::catPath(QString path)
-{
-    int i = path.lastIndexOf(search);
-    i = path.indexOf('/', i);
-    if (i != -1)
-        path.truncate(i);
-    return path;
+    ui->lineEdit->setDisabled(false);
+    ui->pushButton->setText("Search");
+    ui->statusBar->showMessage(QString("Finish search: %1 ms").arg(timer.elapsed()));
 }
 
 void MainWindow::addPath(QString path)
 {
-    QTreeWidgetItem *item, *newparent, *parent = root;
+    QTreeWidgetItem *newparent, *parent = root;
 
     QString currentpath;
 
@@ -102,32 +123,44 @@ void MainWindow::addPath(QString path)
         }
 
         if (newparent != NULL)
-        {
             parent = newparent;
-        }
         else
-        {
-            QFileInfo info(currentpath);
-            QIcon icon = QFileIconProvider().icon(info);
-            QString type = QFileIconProvider().type(info);
-
-            item = new QTreeWidgetItem(parent);
-
-            item->setText(0, dir);
-            item->setIcon(0, icon);
-            item->setData(0, Qt::UserRole, currentpath);
-
-            item->setText(1, type);
-
-            item->setText(2, QString("%1 KB").arg( int((info.size() + 1023) / 1024) ));
-
-            item->setText(3, info.lastModified().toString());
-
-            ui->treeWidget->addTopLevelItem(item);
-            ui->treeWidget->expandItem(item);
-            parent = item;
-        }
+            parent = addItem(parent, currentpath, dir,
+                             (dir.indexOf(search) != -1)
+                             );
     }
+}
+
+QTreeWidgetItem *MainWindow::addItem(QTreeWidgetItem *parent, QString path, QString name, bool mark)
+{
+    QFileInfo info(path);
+
+    QIcon icon = QFileIconProvider().icon(info);
+    QString type = QFileIconProvider().type(info);
+
+    QTreeWidgetItem *item = new QTreeWidgetItem(parent);
+
+    item->setText(0, name);
+    item->setIcon(0, icon);
+    item->setData(0, Qt::UserRole, path);
+    if (mark)
+    {
+        QFont font;
+        font.setBold(true);
+        item->setFont(0, font);
+        item->setTextColor(0, QColor(0, 128, 0));
+    }
+
+    item->setText(1, type);
+
+    item->setText(2, QString("%1 KB").arg( int((info.size() + 1023) / 1024) ));
+
+    item->setText(3, info.lastModified().toString("ddd MMM d, hh:mm"));
+
+    ui->treeWidget->addTopLevelItem(item);
+    ui->treeWidget->expandItem(item);
+
+    return item;
 }
 
 void MainWindow::on_treeWidget_itemDoubleClicked(QTreeWidgetItem* item, int column)
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QTreeWidgetItem>
 #include <QProcess>
+#include <QTime>
 
 namespace Ui {
     class MainWindow;
@@ -19,12 +20,14 @@ protected:
     void changeEvent(QEvent *e);
     QString catPath(QString path);
     void addPath(QString path);
+    QTreeWidgetItem *addItem(QTreeWidgetItem *parent, QString path, QString name, bool mark);
 
 private:
     Ui::MainWindow *ui;
     QTreeWidgetItem *root;
     QProcess *process;
     QString search;
+    QTime timer;
 
 private slots:
     void on_treeWidget_itemDoubleClicked(QTreeWidgetItem* item, int column);
